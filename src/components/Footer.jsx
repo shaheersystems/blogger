e9d51bea65ee95e3eef2eaac2be92752338ccc5d
shaheerinit <105648279+shaheersystems@users.noreply.github.com>
2023-05-28 18:25:40 +0500

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,18 +13,23 @@ function Footer() {
     "Text to speech",
   ];
   return (
-    <div className='border-t py-8 px-8 flex items-center justify-between flex-wrap'>
-      {links.map((link, index) => {
-        return (
-          <a
-            className='text-sm text-gray-600 hover:underline'
-            key={index}
-            href='/'
-          >
-            {link}
-          </a>
-        );
-      })}
+    <div className='flex flex-col py-2'>
+      <div className='border-t py-8 px-8 flex items-center justify-between flex-wrap'>
+        {links.map((link, index) => {
+          return (
+            <a
+              className='text-sm text-gray-600 hover:underline'
+              key={index}
+              href='/'
+            >
+              {link}
+            </a>
+          );
+        })}
+      </div>
+      <div className='flex items-center py-1 justify-center'>
+        <p className='text-sm text-gray-600'>2023 © medium.com</p>
+      </div>
     </div>
   );
 }
